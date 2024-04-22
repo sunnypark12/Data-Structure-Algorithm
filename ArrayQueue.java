7537b04aa7b6ad990ac55eeafb5f24a45c2e9057
sunnypark12 <143741023+sunnypark12@users.noreply.github.com>
2024-04-22 15:28:24 -0400

    Add files via upload

---
+++ ArrayQueue.java
@@ -0,0 +1,139 @@
+
+import java.util.NoSuchElementException;
+/**
+ * Your implementation of an ArrayQueue.
+ *
+ * @author SUNHO PARK
+ * @version 1.0
+ * @userid spark901
+ * @GTID 903795870
+ *
+ * Collaborators: LIST ALL COLLABORATORS YOU WORKED WITH HERE
+ *
+ * Resources: LIST ALL NON-COURSE RESOURCES YOU CONSULTED HERE
+ */
+public class ArrayQueue<T> {
+
+    /*
+     * The initial capacity of the ArrayQueue.
+     *
+     * DO NOT MODIFY THIS VARIABLE.
+     */
+    public static final int INITIAL_CAPACITY = 9;
+
+    /*
+     * Do not add new instance variables or modify existing ones.
+     */
+    private T[] backingArray;
+    private int front;
+    private int size;
+
+    /**
+     * Constructs a new ArrayQueue.
+     */
+    public ArrayQueue() {
+        backingArray = (T[]) new Object[INITIAL_CAPACITY];
+        front = 0;
+        size = 0;
+    }
+
+    /**
+     * Adds the data to the back of the queue.
+     *
+     * If sufficient space is not available in the backing array, resize it to
+     * double the current length. When resizing, copy elements to the
+     * beginning of the new array and reset front to 0.
+     *
+     * Must be amortized O(1).
+     *
+     * @param data the data to add to the back of the queue
+     * @throws java.lang.IllegalArgumentException if data is null
+     */
+    public void enqueue(T data) {
+        if (data == null) {
+            throw new IllegalArgumentException("Null data cannot be added.");
+        }
+        if (size == backingArray.length) {
+            T[] newArray = (T[]) (new Object[backingArray.length * 2]);
+            for (int i = 0; i < backingArray.length; i++) {
+                int idx = (front++) % backingArray.length;
+                newArray[i] = backingArray[idx];
+            }
+            front = 0;
+            backingArray = newArray;
+        }
+        int idx = (front + size) % backingArray.length;
+        backingArray[idx] = data;
+        size++;
+    }
+
+    /**
+     * Removes and returns the data from the front of the queue.
+     *
+     * Do not shrink the backing array.
+     *
+     * Replace any spots that you dequeue from with null.
+     *
+     * If the queue becomes empty as a result of this call, do not reset
+     * front to 0.
+     *
+     * Must be O(1).
+     *
+     * @return the data formerly located at the front of the queue
+     * @throws java.util.NoSuchElementException if the queue is empty
+     */
+    public T dequeue() {
+        if (size == 0) {
+            throw new NoSuchElementException("Cannot remove data from an empty queue.");
+        }
+        T remove = backingArray[front];
+        backingArray[front] = null;
+        front = (front + 1) % backingArray.length;
+        size--;
+        if (size == 0) {
+            front = 0;
+        }
+        return remove;
+    }
+
+    /**
+     * Returns the data from the front of the queue without removing it.
+     *
+     * Must be O(1).
+     *
+     * @return the data located at the front of the queue
+     * @throws java.util.NoSuchElementException if the queue is empty
+     */
+    public T peek() {
+        if (size == 0) {
+            throw new NoSuchElementException("No element can be returned from empty queue.");
+        }
+        return backingArray[front];
+    }
+
+    /**
+     * Returns the backing array of the queue.
+     *
+     * For grading purposes only. You shouldn't need to use this method since
+     * you have direct access to the variable.
+     *
+     * @return the backing array of the queue
+     */
+    public T[] getBackingArray() {
+        // DO NOT MODIFY THIS METHOD!
+        return backingArray;
+    }
+
+    /**
+     * Returns the size of the queue.
+     *
+     * For grading purposes only. You shouldn't need to use this method since
+     * you have direct access to the variable.
+     *
+     * @return the size of the queue
+     */
+    public int size() {
+        // DO NOT MODIFY THIS METHOD!
+        return size;
+    }
+}
